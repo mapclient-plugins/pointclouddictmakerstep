4574b593f4dc4f6946b762ee20c2794800bda533
TimmyNic <tim.nicolas@outlook.com>
2023-07-13 18:29:42 +1200

    Address PEP8 warnings
    Address as many PEP8 warnings as possible.

--- mapclientplugins/pointclouddictmakerstep/step.py
+++ mapclientplugins/pointclouddictmakerstep/step.py
@@ -1,18 +1,18 @@
-'''
+"""
 MAP Client Plugin Step
-'''
+"""
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
 
 
-class pointsCloudDictMakerStep(WorkflowStepMountPoint):
-    '''
+class PointsCloudDictMakerStep(WorkflowStepMountPoint):
+    """
     Skeleton step which is intended to be a helpful starting points
     for new steps.
-    '''
+    """
 
     def __init__(self, location):
-        super(pointsCloudDictMakerStep, self).__init__('points Cloud Dict Maker', location)
+        super(PointsCloudDictMakerStep, self).__init__('points Cloud Dict Maker', location)
         self._configured = True  # A step cannot be executed until it has been configured.
         self._category = 'General'
         # Add any other initialisation code here:
@@ -39,11 +39,11 @@ class pointsCloudDictMakerStep(WorkflowStepMountPoint):
         self._pointsDict2 = None
 
     def execute(self):
-        '''
+        """
         Add your code here that will kick off the execution of the step.
         Make sure you call the _doneExecution() method when finished.  This method
         may be connected up to a button in a widget for example.
-        '''
+        """
         # Put your execute step code here before calling the '_doneExecution' method.
         if self._pointsDict is None:
             self._pointsDict = {}
@@ -55,11 +55,11 @@ class pointsCloudDictMakerStep(WorkflowStepMountPoint):
         self._doneExecution()
 
     def setPortData(self, index, dataIn):
-        '''
+        """
         Add your code here that will set the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         uses port for this step then the index can be ignored.
-        '''
+        """
         if index == 1:
             self._pointsDict = dataIn  # ju#pointclouddict
         elif index == 2:
@@ -70,45 +70,45 @@ class pointsCloudDictMakerStep(WorkflowStepMountPoint):
             self._pointsDict2 = dataIn  # pointclouddict
 
     def getPortData(self, index):
-        '''
+        """
         Add your code here that will return the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         provides port for this step then the index can be ignored.
-        '''
+        """
         return self._pointsDict  # ju#pointsclouddict
 
     def configure(self):
-        '''
+        """
         This function will be called when the configure icon on the step is
         clicked.  It is appropriate to display a configuration dialog at this
         time.  If the conditions for the configuration of this step are complete
         then set:
             self._configured = True
-        '''
+        """
         pass
 
     def getIdentifier(self):
-        '''
+        """
         The identifier is a string that must be unique within a workflow.
-        '''
+        """
         return 'pointsCloudDictMaker'  # TODO: The string must be replaced with the step's unique identifier
 
     def setIdentifier(self, identifier):
-        '''
+        """
         The framework will set the identifier for this step when it is loaded.
-        '''
+        """
         pass  # TODO: Must actually set the step's identifier here
 
     def serialize(self):
-        '''
+        """
         Add code to serialize this step to disk. Returns a json string for
         mapclient to serialise.
-        '''
+        """
         return ''
 
     def deserialize(self, string):
-        '''
+        """
         Add code to deserialize this step from disk. Parses a json string
         given by mapclient
-        '''
+        """
         pass
